3f71e773dce95aab117e68f838168b3f63527353
zwickvitaly <zwickvitaly@gmail.com>
2025-01-17 05:14:31 +0300

    architect_rework

--- backend/celery_main.py
+++ backend/celery_main.py
@@ -24,7 +24,7 @@ celery_app.conf.beat_schedule = {
         "task": "products_data_get",
         "schedule": crontab(
             hour="2",
-            minute="30",
+            minute="20",
         ),
     },
 }
