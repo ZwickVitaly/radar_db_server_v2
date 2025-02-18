ca3d44bb8d8623f71f5bd864fe2a6fea7fa88bf0
Zwick Vitaly <zwickvitaly@gmail.com>
2025-02-18 18:25:19 +0400

    ditch_temp_table

--- backend/celery_main.py
+++ backend/celery_main.py
@@ -31,7 +31,7 @@ celery_app.conf.beat_schedule = {
         "task": "catalog_data_get",
         "schedule": crontab(
             hour="14",
-            minute="07",
+            minute="27",
         ),
     },
 }
